bf7388bc059662c55378e7b77a861f3011bc31d9
mdeboertje <contact.mikedeboer@gmail.com>
2022-05-12 15:07:41 +0200

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module nl.betabit.betabit {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens nl.betabit.betabit to javafx.fxml;
+    exports nl.betabit.betabit;
+}
---
+++ src/main/java/nl/betabit/betabit/HelloController.java
@@ -0,0 +1,14 @@
+package nl.betabit.betabit;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
